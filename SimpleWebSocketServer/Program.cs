9815d7781120e87ea52dccfa9c5b2d85b19ed234
KY <xiaoyang7481@gmail.com>
2024-07-09 14:15:15 +0800

    加入專案檔案。

---
+++ SimpleWebSocketServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5000");
+var app= builder.Build();
+app.UseWebSockets();
+
+var connections = new List<WebSocket>();//裝載連線用戶的容器
+
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var Client = context.Request.Query["name"];
+
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+        connections.Add(ws);
+
+        await Broadcast($"{Client} joined the room");// 廣播訊息
+        await Broadcast($"{connections.Count} users connected");// 廣播訊息
+        await ReceiveMessage(ws,
+            async (result, buffer) =>
+            {
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    await Broadcast(Client + ":" + message);
+                }
+                else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
+                {
+                    connections.Remove(ws);
+                    await Broadcast($"{Client} left the room");
+                    await Broadcast($"{connections.Count} users connected");
+                    await ws.CloseAsync(result.CloseStatus!.Value, result.CloseStatusDescription, CancellationToken.None);
+                }
+            });
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+});
+
+async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)// Server 接收訊息
+{ 
+    var buffer = new byte[1024*4];
+    while (socket.State == WebSocketState.Open)
+    {
+        var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        handleMessage(result, buffer);
+    }
+}
+
+async Task Broadcast(string message)
+{
+    var bytes = Encoding.UTF8.GetBytes(message);
+    foreach (var socket in connections)//透過 Broatcast 的方式發送給每個Client
+    {
+        if (socket.State == WebSocketState.Open)//判斷如果Client 還在連線
+        { 
+            var arraySegment = new ArraySegment<byte>(bytes,0,bytes.Length);
+            await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}
+
+await app.RunAsync();
